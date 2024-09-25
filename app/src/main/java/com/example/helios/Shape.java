7238d1f0c15157ac01fde5c77db61cb32e3a1b35
SirKairon <dhruvkairon@gmail.com>
2024-09-25 11:41:27 -0600

    resolving merge conflict

--- app/src/main/java/com/example/helios/Shape.java
+++ app/src/main/java/com/example/helios/Shape.java
@@ -3,6 +3,6 @@ package com.example.helios;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "Cyan";
+    private String color = "Mike merge conflict";
 
 }
